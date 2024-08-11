e2eb4c8e7667e0bdc8d55a663b6794eca2e0fe27
Ogzhanakc <ogzhan.akc@gmail.com>
2024-08-11 12:16:14 +0300

    appRouting installation

--- lib/pages/home/view/home_view.dart
+++ lib/pages/home/view/home_view.dart
@@ -1,6 +1,9 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_homework_uplide/pages/weather/view/weather_view.dart';
 
+
+@RoutePage()
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
 
--- lib/pages/splash_screen/view/splash_screen_view.dart
+++ lib/pages/splash_screen/view/splash_screen_view.dart
@@ -1,5 +1,8 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 
+
+@RoutePage()
 class SplashScreenView extends StatefulWidget {
   const SplashScreenView({super.key});
 
