c9af4be02691e5095dde38efe1887d89b2611f66
Ogzhanakc <ogzhan.akc@gmail.com>
2024-08-11 12:19:31 +0300

    splash screen init

--- lib/pages/splash_screen/view/splash_screen_view.dart
+++ lib/pages/splash_screen/view/splash_screen_view.dart
@@ -1,18 +1,24 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter_homework_uplide/pages/splash_screen/view_model/splash_screen_view_model.dart';
+import 'package:provider/provider.dart';
 
 @RoutePage()
-class SplashScreenView extends StatefulWidget {
+class SplashScreenView extends StatelessWidget {
   const SplashScreenView({super.key});
 
   @override
-  State<SplashScreenView> createState() => _SplashScreenViewState();
-}
-
-class _SplashScreenViewState extends State<SplashScreenView> {
-  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    Provider.of<SplashScreenViewModel>(context, listen: false).routeToHome(context);
+    return Consumer<SplashScreenViewModel>(
+      builder: (context, viewModel, child){
+        return const Scaffold(
+        body: Center(
+          child: CircularProgressIndicator(),
+        ),
+      );
+      }
+    
+    );
   }
 }
--- lib/pages/splash_screen/view_model/splash_screen_view_model.dart
+++ lib/pages/splash_screen/view_model/splash_screen_view_model.dart
@@ -1,3 +1,12 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_homework_uplide/core/init/routes/app_router.dart';
 
-class SplashScreenViewModel extends ChangeNotifier {}
+class SplashScreenViewModel extends ChangeNotifier {
+  Future<void> routeToHome(BuildContext context) async {
+    await Future.delayed(Duration(seconds: 2));
+    context.router.replace(HomeRoute());
+    notifyListeners();
+  }
+}
