226ba8b39b469df84191542ea7fd75518af632a3
Ogzhanakc <ogzhan.akc@gmail.com>
2024-08-11 13:24:19 +0300

    cache manager installed

---
+++ lib/core/init/cache/cache_manager.dart
@@ -0,0 +1,37 @@
+import 'package:flutter_homework_uplide/core/constants/enums/cache_keys.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+
+class CacheManager {
+  SharedPreferences? _preferences;
+
+  CacheManager() {
+    _initPreferences();
+  }
+
+  Future<void> _initPreferences() async {
+    _preferences = await SharedPreferences.getInstance();
+  }
+
+  Future<void> setStringValue(CacheKeys key, String value) async {
+    await _preferences!.setString(key.toString(), value);
+  }
+
+
+  Future<void> setStringList(CacheKeys key, List<String> value) async {
+
+    await _preferences!.setStringList(key.toString(), value);
+  }
+
+  Future<String> getStringValue(CacheKeys key) async {
+
+    return _preferences?.getString(key.toString()) ?? '';
+  }
+
+  Future<List<String>> getStringList(CacheKeys key) async {
+
+    return _preferences?.getStringList(key.toString()) ?? [];
+  }
+
+
+}
--- lib/core/init/get_it/app_get_it.dart
+++ lib/core/init/get_it/app_get_it.dart
@@ -1,9 +1,10 @@
-  import 'package:flutter_homework_uplide/pages/weather/service/weather_service.dart';
+import 'package:flutter_homework_uplide/core/init/cache/cache_manager.dart';
+import 'package:flutter_homework_uplide/pages/weather/service/weather_service.dart';
 import 'package:get_it/get_it.dart';
 
 final locator = GetIt.instance;
 
-  void setupLocator() {
- locator.registerFactory<WeatherService>(() => WeatherService());
-
-  }
+void setupLocator() {
+  locator.registerFactory<WeatherService>(() => WeatherService());
+  locator.registerFactory<CacheManager>(() => CacheManager());
+}
