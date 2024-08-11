eb90869a3a9f356bb7501d3c4d8f9e51a83f4708
Ogzhanakc <ogzhan.akc@gmail.com>
2024-08-11 12:22:40 +0300

    get_it added

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_homework_uplide/core/init/get_it/app_get_it.dart';
 import 'package:flutter_homework_uplide/core/init/routes/app_router.dart';
+import 'package:flutter_homework_uplide/pages/splash_screen/view_model/splash_screen_view_model.dart';
+import 'package:flutter_homework_uplide/pages/weather/view_model/weather_view_model.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(MyApp());
+  setupLocator();
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => WeatherViewModel()),
+        ChangeNotifierProvider(create: (_) => SplashScreenViewModel()),
+      ],
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -17,4 +30,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
